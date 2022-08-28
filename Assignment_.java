cb9277a6af1a7ef91d330bd45c2a55d6fa734fc0
AshvinBari <98014877+AshvinBari@users.noreply.github.com>
2022-08-28 21:29:40 +0530

    Add files via upload

---
+++ Assignment_.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Scanner;
+public class Assignment_ {
+
+	public static void main(String[] args) 
+	
+	{
+		int i = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Sentence: ");
+        String sentence = scanner.nextLine();
+        System.out.print("Enter a Letter: ");
+        char letter = scanner.next().charAt(0);
+        scanner.close();
+
+        for (i = 0; i < sentence.length(); i++) {
+            if (sentence.charAt(i) == letter) {
+                break;
+            }
+        }
+        if (i < sentence.length() - 1) {
+            System.out.print(sentence.substring(i + 1));
+        } else {
+            System.out.print("The letter is worng ");
+        }
+
+	}
+
+}
